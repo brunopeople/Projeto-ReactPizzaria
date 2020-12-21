cc90b05a5d48cef49fbc9c6e7d0afe8e64ff0b68
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2020-12-21 16:13:18 -0300

    criando o componente Featured

---
+++ websiste-pizzaria/src/Features/Featuread.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FeatureContainer, FeatureButton } from './FeatureElements';
+
+const Feature = () => {
+    return (
+        <FeatureContainer>
+            <h1>A Pizza do Dia!</h1>
+            <p> Molho de Tufra regado de pó de ouro de 24 quilates</p>
+            <FeatureButton>Peça Agora! </FeatureButton>
+        </FeatureContainer>
+    );
+};
+
+export default Feature;
